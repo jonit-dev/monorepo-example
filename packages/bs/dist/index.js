7ed630e7e15573e3f91c38baae4f110bc3493ac2
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-08-08 22:10:18 -0700

    monorepo example

--- packages/bs/dist/index.js
+++ packages/bs/dist/index.js
@@ -1,14 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BSGenerator = void 0;
-const lodash_1 = __importDefault(require("lodash"));
 class BSGenerator {
 }
 exports.BSGenerator = BSGenerator;
 BSGenerator.generateRandomBS = () => {
     const BSList = ['Organic food is healthier and good to the environemnt', 'Trump is a great leader', 'Earth is flat'];
-    return lodash_1.default.sample(BSList);
+    return _.sample(BSList);
 };
